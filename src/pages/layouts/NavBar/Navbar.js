815ec48cf83a8e3de2aaca99f71b45df726516c0
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-08-20 11:30:40 -0300

    Fix context

--- src/pages/layouts/NavBar/Navbar.js
+++ src/pages/layouts/NavBar/Navbar.js
@@ -14,22 +14,22 @@ export function NavBar(){
                 <button onclick="openMenu()"><img title="Entrar" src="../imgs/menu.png" alt=""/></button>
             </li>
             <li class="navbar-links">
-                <Link target="_parent" href="../Home/index.html">Início</Link>
+                <Link to="/" target="_parent" href="../Home/index.html">Início</Link>
             </li>
             <li class="navbar-links">
-                <Link target="_parent" href="../Produtos/produtos.html">Produtos</Link>
+                <Link to="/produto" target="_parent" href="../Produtos/produtos.html">Produtos</Link>
             </li>
             <li class="navbar-links">
-                <Link target="_parent" href="../CadastrarProduto/Index.html">Cadastrar Produtos</Link>
+                <Link to="/cadastrarProduto" target="_parent" href="../CadastrarProduto/Index.html">Cadastrar Produtos</Link>
             </li>
             <li class="navbar-links">
-                <Link target="_parent" href="../Sobre/sobre.html">Sobre</Link>
+                <Link to="/sobre" target="_parent" href="../Sobre/sobre.html">Sobre</Link>
             </li>
             <li class="navbar-links">
-                <Link target="_parent" href="../Carrinho/index.html">Carrinho</Link>
+                <Link to="/carrinho" target="_parent" href="../Carrinho/index.html">Carrinho</Link>
             </li>
             <li class="navbar-links">
-                <Link target="_parent" href="../Login/login.html">Conta</Link>
+                <Link to="/login" target="_parent" href="../Login/login.html">Conta</Link>
             </li>
         </ul>
     </nav>
